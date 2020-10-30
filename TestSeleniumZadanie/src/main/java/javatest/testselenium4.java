dc814eed8f57ce848b7eaa107be51b55b81fcdf4
lukasztraczyk <72923285+lukasztraczyk@users.noreply.github.com>
2020-10-30 23:21:26 +0100

    Update testselenium4.java

--- TestSeleniumZadanie/src/main/java/javatest/testselenium4.java
+++ TestSeleniumZadanie/src/main/java/javatest/testselenium4.java
@@ -11,8 +11,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -36,14 +34,7 @@ public class testselenium4 {
         if (browser.equalsIgnoreCase("FF")) {
             String dostepDoFirefox = "/home/greg/geckodriver";
             System.setProperty("webdriver.gecko.driver", dostepDoFirefox);
-            FirefoxProfile profile = new FirefoxProfile();
-            profile.setPreference("browser.download.folderList", 2);
-            profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/zip");
-            profile.setPreference("browser.download.manager.showWhenStarting", false);
-            profile.setPreference("pdfjs.disabled", true);
-            FirefoxOptions opt = new FirefoxOptions();
-            opt.setProfile(profile);//ustawiam w ustawieniach Firefox że w przypadku pobierania plików typu application/zip nie wyskakuje okienko z pytaniem czy chce się zobaczyć pobrany plik czy zapisać
-            driver = new FirefoxDriver(opt);
+                driver = new FirefoxDriver();
         }
         else
         if (browser.equalsIgnoreCase("Chrome")) {
@@ -53,7 +44,7 @@ public class testselenium4 {
         }
         driver.manage().window().maximize();
         driver.get("https://www.medicalgorithmics.pl/");
-        Thread.sleep(2000);
+        TimeUnit.MILLISECONDS.sleep(1555);
     }
 
     @Test
@@ -61,20 +52,19 @@ public class testselenium4 {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         String jsCommand = "return document.readyState";
 
-        Assert.assertEquals("complete", js.executeScript(jsCommand).toString());//Upewnij się, że strona Medicalgorithmics załadowała i wyświetla się poprawnie - zaimplementuj 2 sposoby
-        Assert.assertFalse("Timeout waiting for Page Load Request to complete.", false);//Upewnij się, że strona Medicalgorithmics załadowała i wyświetla się poprawnie - zaimplementuj 2 sposoby
+        Assert.assertEquals("complete", js.executeScript(jsCommand).toString());//Upewnij się, że strona Medicalgorithmics załadowała i wyświetla się poprawnie
+        Assert.assertFalse("Timeout waiting for Page Load Request to complete.", false);
         Assert.assertEquals("https://www.medicalgorithmics.pl/",driver.getCurrentUrl());
-
-        driver.findElement(By.xpath("/html/body/div[3]/div/header/div/div/div/div[2]/div/div/a")).click();
+        TimeUnit.MILLISECONDS.sleep(1555);
+        driver.findElement(By.cssSelector(".icon_search")).click();
         driver.findElement(By.className("qode_search_field")).sendKeys("Pocket ECG CRS");//Wyszukaj frazę: “Pocket ECG CRS”, w wyszukiwarce znajdującej się na stronie
         driver.findElement(By.className("qode_search_field")).submit();
-        TimeUnit.MILLISECONDS.sleep(4555);
+        TimeUnit.MILLISECONDS.sleep(2555);
         List<WebElement> links = driver.findElements(By.className("post_date"));//każdy wynik ma datę dodania, po niej wyszukuję
-        TimeUnit.MILLISECONDS.sleep(1555);
         List<WebElement> jedna_strona = driver.findElements(By.linkText("PocketECG CRS – telerehabilitacja kardiologiczna"));
-        TimeUnit.MILLISECONDS.sleep(1555);
         List<WebElement> nieaktywnelinki = driver.findElements(By.className("inactive"));//liczę liczbę nieaktywnych stron
-        int sumalinkow = nieaktywnelinki.size()+1;//zakładam że zawsze jestem na jednej aktywnej stronie
+        List<WebElement> aktywne = driver.findElements(By.className("active"));
+        int sumalinkow = nieaktywnelinki.size()+aktywne.size();
 
         Assert.assertEquals(10, links.size());// Zweryfikuj, że wyszukiwanie po powyższej frazie zwraca dokładnie 10 wyników na pierwszej stronie.
         Assert.assertEquals(1, jedna_strona.size());// Zweryfikuj, że wynik wyszukiwania zwraca dokładnie 1 rezultat, w którym znajduje się fraza “PocketECG CRS – telerehabilitacja kardiologiczna"
@@ -82,5 +72,3 @@ public class testselenium4 {
 
    }
 }
-
-
